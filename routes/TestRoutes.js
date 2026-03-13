5822b10f34bcfd2d78a1bcb0a77aff9c5d9302c4
nessa-7 <yisel7948@gmail.com>
2026-03-13 12:09:57 -0500

    inseguridad

--- routes/TestRoutes.js
+++ routes/TestRoutes.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const TestController = require("../controllers/TestController");
 
-const verificarToken = require("../middleware/authMiddleware")
 
-router.post("/pretest", verificarToken, TestController.pretest);
-router.post("/start", verificarToken, TestController.start);
-router.post("/next-question", verificarToken, TestController.nextQuestion);
-router.post("/answer", verificarToken, TestController.saveAnswer);
-router.post("/finish", verificarToken, TestController.finalizar);
+router.post("/pretest", TestController.pretest);
+router.post("/start", TestController.start);
+router.post("/next-question", TestController.nextQuestion);
+router.post("/answer", TestController.saveAnswer);
+router.post("/finish", TestController.finalizar);
 
 module.exports = router;
